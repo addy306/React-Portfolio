567f426c50d21ac9a26d8492299f2c1be2a0226b
Your Name <you@example.com>
2024-02-07 00:18:16 +0000

    Adding images and homepage details

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import addybabepic from '/AddybabePic.jpg'
 
 function Home() {
     return(
-       <h1>Thi is the homepage</h1>
+        <div className='flex justify-center flex-col items-center'>
+        <div className='h-[120px] border-r-[1px] mt-[-20px]'/>
+        <div className='w-[5px] h-[5px] bg-green-600 rounded-full'></div>
+        <h2 className='mt-5 text-black font-medium text-[13px]
+        tracking-widest'>HELLO! MY NAME IS</h2>
+        <h2 className='text-[70px] font-bold text-black tracking-widest
+        mt-5'>ADIJAT <br></br>SALISU</h2>
+        <img src={addybabepic}
+        className='w-[160px] h-[160xp] bg-gray-200 p-7 rounded-full'
+        />
+    </div>
     )
 }
 
