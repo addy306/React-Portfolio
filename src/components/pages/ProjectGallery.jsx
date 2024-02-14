565f5b9c3cad4c1a4990f7db4bd228da218b7d72
Your Name <you@example.com>
2024-02-14 10:02:27 +0000

    Adding details and images to project gallery

--- src/components/pages/ProjectGallery.jsx
+++ src/components/pages/ProjectGallery.jsx
@@ -7,6 +7,7 @@ const ProjectGallery = () => {
   return (
     <div>
       <h1>Project Gallery</h1>
+      <a href="https://github.com/addy306">Visit Github Repo for more</a>
       <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">  
     <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
       {projectsData.map((project) => (
--- src/components/pages/Skills.jsx
+++ src/components/pages/Skills.jsx
@@ -3,6 +3,8 @@ import bootstrap from '/bootstrap-portfolio.jpg'
 
 function Skills() {
     return(
+        <div>
+            <h1>Skills</h1>
         <div className="max-w-screen-xl mx-auto p-5 sm:p-10 md:p-16">
         <div className="grid grid-cols-1 md:grid-cols-3 sm:grid-cols-2 gap-10">
             <div
@@ -64,7 +66,56 @@ function Skills() {
                    
                 </div>
             </div>
+
+            <div
+                className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="/photoshop.jpg" alt="html,css and javascript"/>
+                <div className="p-4 pt-2">
+                    <div className="mb-8">
+                        
+                        <ul>
+                            <li> Photoshop</li>
+                            
+                        </ul>
+                    </div>
+                   
+                </div>
+            </div>
+
+            <div
+                className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="/Wordpress_woocommerce.png" alt="html,css and javascript"/>
+                <div className="p-4 pt-2">
+                    <div className="mb-8">
+                        
+                        <ul>
+                            <li> Wordpress</li>
+                            <li> Wordpress Themes & Plugins</li>
+                            <li> Woocommerce</li>
+                            <li> Woocommerce Payment Intergration</li>
+                        </ul>
+                    </div>
+                   
+                </div>
+            </div>
+
+            <div
+                className="border-r border-b border-l border-gray-400 lg:border-t lg:border-gray-400 bg-white rounded-b lg:rounded-b-none lg:rounded-r flex flex-col justify-between leading-normal">
+                <img src="/bootstrap_tailwind.png" alt="html,css and javascript"/>
+                <div className="p-4 pt-2">
+                    <div className="mb-8">
+                        
+                        <ul>
+                            <li> Bootstrap</li>
+                            <li> Tailwind</li>
+                            
+                        </ul>
+                    </div>
+                   
+                </div>
+            </div>
         </div>
+    </div>
     </div>
     )
 }
